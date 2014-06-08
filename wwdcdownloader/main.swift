01340dda6f483f50b5c69f67c1393ea0c14c5551
Les Stroud <les@lesstroud.com>
2014-06-08 11:57:43 -0400

    Initial Commit

---
+++ wwdcdownloader/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  wwdcdownloader
+//
+//  Created by Les Stroud on 6/8/14.
+//  Copyright (c) 2014 Les Stroud. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+

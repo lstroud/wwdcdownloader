ddbf8aa49bb84795f9925b03122e82ac6b185e86
Les Stroud <les@lesstroud.com>
2014-06-10 09:27:45 -0400

    just working through understanding swift
    this is not yet functional…

--- wwdcdownloader/main.swift
+++ wwdcdownloader/main.swift
@@ -8,5 +8,68 @@
 
 import Foundation
 
-println("Hello, World!")
+
+
+func parseJSON(data:NSData) -> Dictionary<String, AnyObject>?{
+    var error: NSError?
+    var json = NSJSONSerialization.JSONObjectWithData(data, options:NSJSONReadingOptions.AllowFragments, error:&error) as? Dictionary<String, AnyObject>
+    
+    if let dict = json {
+        return json;
+    } else {
+        println("Unable to parse json from data provided.")
+        println(error)
+        return nil
+    }
+}
+
+func getJSON(location:String!, completion:(NSData)->Void){
+    let url = NSURL(string:location)
+    var data = NSData(contentsOfURL: url)
+    completion(data)
+}
+
+func getJSONBackground(location:String!, completion:(NSData)->Void){
+    //var data = NSData.dataWithContentsOfFile("/Users/les/Documents/development/repos/github/wwdcdownloader/wwdcdownloader/wwdcdownloader/testdata.json", options: .DataReadingMappedIfSafe, error: nil)
+    var done = false
+    let session = NSURLSession.sharedSession()
+
+    let itunesJson = session.dataTaskWithURL(url, completionHandler: {(data: NSData!, response: NSURLResponse!, error: NSError!) in
+        //    var json = NSJSONSerialization.JSONObjectWithData(data!, options: 0, error: nil)
+        //println(data)
+        println("Received result")
+        completion(data)
+        done = true
+    })
+
+    itunesJson.resume()
+
+    while (itunesJson.state == NSURLSessionTaskState.Running) && !done{
+        NSThread.sleepForTimeInterval(0.1)
+    }
+}
+
+let url = NSURL(string:"https://itunes.apple.com/search?term=apple&media=software")
+//getJSON("https://itunes.apple.com/search?term=apple&media=software", {(data:NSData) -> Void in
+//        if let json = parseJSON(data){
+//            println(json.description)
+//        } else {
+//            println("Failed")
+//        }
+//
+//    }
+//)
+getJSONBackground("https://itunes.apple.com/search?term=apple&media=software", {(data:NSData) -> Void in
+    if let json = parseJSON(data){
+        println(json.description)
+    } else {
+        println("Failed")
+    }
+    
+    }
+)
+
+
+
+
 
